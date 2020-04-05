e641cdc24f449fff4b212bbefa5b3c2ae394773a
Patryk Stawik <patrykstawik@gmail.com>
2020-04-05 19:10:04 +0200

    finall commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-function App() {
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+
+import NavBar from './components/Navbar'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Router>
+    <NavBar/>
+    <Switch>
+    <Route path="/" exact>
+      <Home/>
+    </Route>
+    <Route path="/about">
+      <About/>
+    </Route>
+    <Route path="/cocktail/:id">
+      <SingleCocktail/>
+    </Route>
+    <Route path="*">
+      <Error/>
+    </Route>
+    </Switch>
+  </Router>
+  )
 }
-
-export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">
+        about us
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus nemo aspernatur pariatur corporis, molestiae, magnam dicta iusto aliquid sunt ullam quis ipsa unde consectetur, sapiente temporibus quasi rem rerum odit.</p>
+    </section>
+  )
+}
